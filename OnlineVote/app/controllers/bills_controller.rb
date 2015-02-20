1b399c5568d9d05ee1021c43f0c7a020f8233d7a
Jonathan Parrish <yoda.peh@gmail.com>
2015-02-20 15:32:10 -0500

    fix strong params bills

--- OnlineVote/app/controllers/bills_controller.rb
+++ OnlineVote/app/controllers/bills_controller.rb
@@ -69,6 +69,6 @@ class BillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def bill_params
-      params.require(:bill).permit(:name, :email, :password_digest)
+      params.require(:bill).permit(:staffer_id, :bill_text, :vote_id)
     end
 end
